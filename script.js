980917bc2178cce6fd7d22a39ee7fe759c9bb1b6
Kalonji <kalonjikm@gmail.com>
2024-01-29 17:52:23 -0800

    Added html and js files to repo

---
+++ script.js
@@ -0,0 +1,10 @@
+var firstName;
+firstName = "Kalonji";
+var lastName;
+lastName = "Mashariki";
+var age;
+age = 23
+
+alert("My name is " + firstName + " " + lastName);
+alert("I am " + age + " years old");
+console.log("Great course 10/10 :~)");
